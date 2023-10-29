b5f3cc76abcfb57dd58b0a557d9ed0c76be4a045
kazimehedihasan <kazimehedihasan458@.com>
2023-10-29 14:10:15 +0600

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId,  } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port =process.env.PORT || 5000;
@@ -9,8 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 
-// EkAm2cZcQOqZwOIs
-// car-doctors
 
 
 
@@ -18,9 +16,7 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.BD_USER}:${process.env.BD_PASS}@cluster0.jkcpjnv.mongodb.net/?retryWrites=true&w=majority`;
 
 
-// const uri = "mongodb+srv://car-doctors:EkAm2cZcQOqZwOIs@cluster0.jkcpjnv.mongodb.net/?retryWrites=true&w=majority";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -33,6 +29,72 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+//code 
+const serviceCollection = client.db('carDoctor').collection('services');
+
+
+const CheckOutCollection = client.db('carDoctor').collection('checkout');
+// all data lod home page a
+app.get('/services', async(req, res) => {
+const cursor = serviceCollection.find();
+const result = await cursor.toArray();
+res.send(result);
+})
+
+// home page theke click korle lod hobe
+app.get('/services/:id', async (req, res) => {
+const id = req.params.id;
+const query = {_id: new ObjectId(id)}
+const options = {
+  projection: { title: 1, price: 1, service_id: 1 , img: 1},
+};
+const result = await serviceCollection.findOne(query, options);
+res.send(result)
+})
+
+
+// CheckOut data mongoose a add korar jonno clint sit a theke 
+app.post('/checkout', async (req, res) => {
+  const Check = req.body;
+  const result = await CheckOutCollection.insertOne(Check);
+  res.send(result);
+},[])
+
+// add korar kore dekhabo
+app.get('/checkout', async (req, res) => {
+  console.log(req.query.email);
+  let query = {}
+  if (req.query?.email) {
+    query ={email:req.query.email}
+  }
+  const result = await CheckOutCollection.find(query).toArray();
+  res.send(result);
+})
+
+// delete 
+app.delete('/checkout/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id)};
+  const result = await CheckOutCollection.deleteOne(query);
+  res.send(result);
+}) 
+
+//  updete
+app.patch('/checkout/:id', async(req, res)=>{
+  const id = req.params.id;
+  const filter = {_id: new ObjectId(id)};
+const updeteCheckout = req.body;
+console.log(updeteCheckout);
+const updeteDoc = {
+  $set:{
+    status:updeteCheckout.status
+  },
+};
+const result = await CheckOutCollection.updeteOne(filter, updeteDoc);
+res.send(result);
+})
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
